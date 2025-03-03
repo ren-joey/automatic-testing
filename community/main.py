4f44c8f9f0b83b96df76406aa1a4a12a9e1b55ab
ren-joey <tsuyiren@gmail.com>
2025-03-03 09:07:35 +0800

    feat: add community testing structure

--- community/main.py
+++ community/main.py
@@ -11,6 +11,15 @@ def get_chrome_driver() -> WebDriver:
     service = Service(ChromeDriverManager().install())
     return webdriver.Chrome(service=service)
 
+def get_element(driver: WebDriver, xpath: str, retry: int = 3):
+    try:
+        return driver.find_element(By.XPATH, xpath)
+    except Exception as e:
+        print(f"Error: {e}")
+        print("Retry after 3 seconds")
+        time.sleep(retry)
+        return get_element(driver, xpath)
+
 def click_element(driver: WebDriver, xpath: str, retry: int = 3):
     try:
         driver.find_element(By.XPATH, xpath).click()
@@ -20,10 +29,22 @@ def click_element(driver: WebDriver, xpath: str, retry: int = 3):
         time.sleep(retry)
         click_element(driver, xpath)
 
+def traverse_communities(driver: WebDriver):
+    ul = get_element(driver, "//div[@class='Community_CardList']/div[@class='dui-emptyHelper']/ul")
+    print("ul.text")
+    print(ul.text)
+
+    li_elements = ul.find_elements(By.XPATH, "./li")
+
+    for li in li_elements:
+        print("li.text")
+        print(li.text)
+
 def start_automatic_testing(driver: WebDriver):
     # Open the website
     driver.get("https://devdms.deltaww.com/communityweb/en-us/Community/")
     click_element(driver, "//button[@class='ant-btn ant-btn-primary']")
+    traverse_communities(driver)
     time.sleep(60)
     driver.quit()
 
